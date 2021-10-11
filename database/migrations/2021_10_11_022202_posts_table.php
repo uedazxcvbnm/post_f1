3d0e9f45a5eebd842a2326e15655dbcf036faf64
nagata127 <kd1305813@st.kobedenshi.ac.jp>
2021-10-11 12:22:28 +0900

    migraton

---
+++ database/migrations/2021_10_11_022202_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->morphs('user');
+            $table->string('name', 255);
+            $table->text('contents');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
